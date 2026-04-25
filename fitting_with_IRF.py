a112157c9ce4a15f4251dc213fa60676f479073b
EVITA <spalinskaiteevita@gmail.com>
2026-04-26 02:26:54 +0300

    Update fitting_with_IRF.py

--- fitting_with_IRF.py
+++ fitting_with_IRF.py
@@ -115,16 +115,10 @@ crop = df[(df[c1]>start) & (df[c1]<end)]
 irf_x = np.array(crop[c1]-min(crop[c1]))
 irf_y = np.array(crop[c2]/max(crop[c2]))
 
-st.write("max difference:", np.max(np.abs(data_x - irf_x[:len(data_x)])))
-st.write("data max:", data_x.max())
-st.write("irf max:", irf_x.max())
-st.write("any above:", np.any(data_x > irf_x.max()))
-st.write("any below:", np.any(data_x < irf_x.min()))
-
 irf_interp = interp1d(irf_x, irf_y)
+data_x = np.clip(data_x, irf_x.min(), irf_x.max())
 irf_yn = irf_interp(data_x)
 irf_yn = irf_yn/max(irf_yn)
-
 
 
 
